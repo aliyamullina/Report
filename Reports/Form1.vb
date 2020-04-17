c39e8e3a428423eb704cd092b4ec648dbf32c85f
Aliya Mullina <aliya.mullina@ya.ru>
2020-04-17 12:00:13 +0300

    error ListBox3 read lines

--- Reports/Form1.vb
+++ Reports/Form1.vb
@@ -6,7 +6,6 @@ Public Class Form1
     Private Sub FileOpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FileOpenToolStripMenuItem.Click
 
         Try
-            'set the openfiledialog properties
             openfile = New OpenFileDialog With {
                 .Title = "Select files",
                 .CheckFileExists = True,
@@ -17,14 +16,16 @@ Public Class Form1
             If openfile.ShowDialog = Windows.Forms.DialogResult.OK Then
                 openfile.SafeFileNames.Count()
                 For i As Integer = 0 To openfile.SafeFileNames.Count() - 1
-                    ListBox1.Items.Add(openfile.SafeFileNames(i))
-                    ListBox2.Items.Add(openfile.FileNames(i))
-
+                    Dim vName = ListBox1.Items.Add(openfile.SafeFileNames(i))
+                    Dim vPath = ListBox2.Items.Add(openfile.FileNames(i))
+                    Dim vReport = ListBox3.Items.AddRange(File.ReadAllLines(vPath))
                 Next
             End If
+
         Catch ex As Exception
             MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End Try
+
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
