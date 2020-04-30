7a639a74fe4ec6b80a4392c818d802d374b7654b
Aliya Mullina <aliya.mullina@ya.ru>
2020-04-30 11:41:21 +0300

    fix clear

--- Reports/Form1.vb
+++ Reports/Form1.vb
@@ -71,7 +71,7 @@ Public Class Form1
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         ListBox1.Items.Clear()
         ListBox2.Items.Clear()
-        ListView1.Clear()
+        ListView1.Items.Clear()
     End Sub
 
 
