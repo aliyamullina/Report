1450a3e16d87e11e6f42cfd2351883db58600200
Aliya Mullina <aliya.mullina@ya.ru>
2020-07-06 09:36:53 +0300

    fix sort

--- Reports/Form1.Designer.vb
+++ Reports/Form1.Designer.vb
@@ -30,9 +30,6 @@ Partial Class Form1
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.ListView1 = New System.Windows.Forms.ListView()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -110,38 +107,11 @@ Partial Class Form1
         Me.ListView1.TabIndex = 6
         Me.ListView1.UseCompatibleStateImageBehavior = False
         '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(442, 305)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(199, 22)
-        Me.TextBox1.TabIndex = 7
-        Me.TextBox1.Text = "OK"
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(442, 333)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(199, 22)
-        Me.TextBox4.TabIndex = 8
-        Me.TextBox4.Text = "нет ответа"
-        '
-        'TextBox5
-        '
-        Me.TextBox5.Location = New System.Drawing.Point(442, 361)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(199, 22)
-        Me.TextBox5.TabIndex = 9
-        Me.TextBox5.Text = "открыть порт"
-        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(678, 442)
-        Me.Controls.Add(Me.TextBox5)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ListView1)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
@@ -166,7 +136,4 @@ Partial Class Form1
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents ListView1 As ListView
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
 End Class
